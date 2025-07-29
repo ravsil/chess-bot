65cea9d7087e969c46a491396ba067ce9619e5de
ravsil <rafaelvieiradas@gmail.com>
2025-07-28 21:42:04 -0300

    added castling

--- game/game.go
+++ game/game.go
@@ -3,11 +3,19 @@ package game
 import "fmt"
 
 type Game struct {
-	Board           Board
-	WhitePieces     []Piece
-	BlackPieces     []Piece
-	WhiteKing       *King
-	BlackKing       *King
+	Board       Board
+	WhitePieces []Piece
+	BlackPieces []Piece
+	WhiteKing   *King
+	BlackKing   *King
+	WhiteRooks  struct {
+		Left  *Rook
+		Right *Rook
+	}
+	BlackRooks struct {
+		Left  *Rook
+		Right *Rook
+	}
 	ValidWhiteMoves map[uint64]uint64
 	ValidBlackMoves map[uint64]uint64
 	CurrentTurn     PColor
@@ -20,6 +28,10 @@ func (g *Game) NewGame() {
 	g.BlackPieces = nil
 	g.WhiteKing = nil
 	g.BlackKing = nil
+	g.WhiteRooks.Left = nil
+	g.WhiteRooks.Right = nil
+	g.BlackRooks.Left = nil
+	g.BlackRooks.Right = nil
 	g.ValidWhiteMoves = nil
 	g.ValidBlackMoves = nil
 
@@ -29,11 +41,25 @@ func (g *Game) NewGame() {
 	for _, pos := range GetBits(g.Board.BlackPawns) {
 		g.BlackPieces = append(g.BlackPieces, &Pawn{Color: BLACK, Pos: (1 << pos)})
 	}
-	for _, pos := range GetBits(g.Board.WhiteRooks) {
-		g.WhitePieces = append(g.WhitePieces, &Rook{Color: WHITE, Pos: (1 << pos)})
+	for i, pos := range GetBits(g.Board.WhiteRooks) {
+		rook := &Rook{Color: WHITE, Pos: (1 << pos)}
+		g.WhitePieces = append(g.WhitePieces, rook)
+		switch i {
+		case 0:
+			g.WhiteRooks.Right = rook
+		case 1:
+			g.WhiteRooks.Left = rook
+		}
 	}
-	for _, pos := range GetBits(g.Board.BlackRooks) {
-		g.BlackPieces = append(g.BlackPieces, &Rook{Color: BLACK, Pos: (1 << pos)})
+	for i, pos := range GetBits(g.Board.BlackRooks) {
+		rook := &Rook{Color: BLACK, Pos: (1 << pos)}
+		g.BlackPieces = append(g.BlackPieces, rook)
+		switch i {
+		case 0:
+			g.BlackRooks.Right = rook
+		case 1:
+			g.BlackRooks.Left = rook
+		}
 	}
 	for _, pos := range GetBits(g.Board.WhiteKnights) {
 		g.WhitePieces = append(g.WhitePieces, &Knight{Color: WHITE, Pos: (1 << pos)})
@@ -69,13 +95,60 @@ func (g *Game) GetValidMoves(color PColor) map[uint64]uint64 {
 		for _, piece := range g.WhitePieces {
 			moves[piece.GetPos()] = piece.GetValidMoves(g.Board)
 		}
+		if !g.WhiteKing.Moved {
+			l := uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01000000)
+			if moves[g.WhiteKing.Pos]&l != 0 && (g.WhiteRooks.Left == nil || g.WhiteRooks.Left.Moved) {
+				moves[g.WhiteKing.Pos] &^= l
+			}
+			r := uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000100)
+			if moves[g.WhiteKing.Pos]&r != 0 && (g.WhiteRooks.Right == nil || g.WhiteRooks.Right.Moved) {
+				moves[g.WhiteKing.Pos] &^= r
+			}
+		}
 	} else {
 		for _, piece := range g.BlackPieces {
 			moves[piece.GetPos()] = piece.GetValidMoves(g.Board)
 		}
+		if !g.BlackKing.Moved {
+			l := uint64(0b01000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000)
+			if moves[g.BlackKing.Pos]&l != 0 && (g.BlackRooks.Left == nil || g.BlackRooks.Left.Moved) {
+				moves[g.BlackKing.Pos] &^= l
+			}
+			r := uint64(0b00000100_00000000_00000000_00000000_00000000_00000000_00000000_00000000)
+			if moves[g.BlackKing.Pos]&r != 0 && (g.BlackRooks.Right == nil || g.BlackRooks.Right.Moved) {
+				moves[g.BlackKing.Pos] &^= r
+			}
+		}
 	}
 	return moves
 }
+
+func (g *Game) handleCastling(newPos uint64, color PColor) {
+	if color == WHITE {
+		if newPos == uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01000000) {
+			if g.WhiteRooks.Left != nil && !g.WhiteRooks.Left.Moved {
+				g.WhiteRooks.Left.Move(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00100000, &g.Board)
+			}
+		}
+		if newPos == uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000100) {
+			if g.WhiteRooks.Right != nil && !g.WhiteRooks.Right.Moved {
+				g.WhiteRooks.Right.Move(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00001000, &g.Board)
+			}
+		}
+	} else {
+		if newPos == uint64(0b01000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000) {
+			if g.BlackRooks.Left != nil && !g.BlackRooks.Left.Moved {
+				g.BlackRooks.Left.Move(0b00100000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, &g.Board)
+			}
+		}
+		if newPos == uint64(0b00000100_00000000_00000000_00000000_00000000_00000000_00000000_00000000) {
+			if g.BlackRooks.Right != nil && !g.BlackRooks.Right.Moved {
+				g.BlackRooks.Right.Move(0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, &g.Board)
+			}
+		}
+	}
+}
+
 func (g *Game) Move(pos uint64, newPos uint64, color PColor) error {
 	if color != g.CurrentTurn {
 		return fmt.Errorf("not %v's turn", color)
@@ -97,6 +170,10 @@ func (g *Game) Move(pos uint64, newPos uint64, color PColor) error {
 
 	for _, p := range it {
 		if p.GetPos() == pos {
+			if p.GetType() == KING && !p.(*King).Moved {
+				g.handleCastling(newPos, color)
+			}
+
 			p.Move(newPos, &g.Board)
 			g.ChangeTurn()
 			g.RemovePiece(newPos, c) // go ?
--- game/king.go
+++ game/king.go
@@ -32,7 +32,43 @@ func (p *King) GetValidMoves(b Board) uint64 {
 		}
 	}
 
-	// TODO: castling
+	if !p.Moved {
+		if p.Color == WHITE {
+			lk1 := uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00100000)
+			lk2 := uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01000000)
+			if (lk1|lk2)&ocupied == 0 &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, lk1, b, KING, WHITE) &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, lk2, b, KING, WHITE) {
+				moves |= lk2
+			}
+			rk1 := uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00001000)
+			rk2 := uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000100)
+			rk3 := uint64(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000010)
+			if (rk1|rk2|rk3)&ocupied == 0 &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, rk1, b, KING, WHITE) &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, rk2, b, KING, WHITE) &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, rk3, b, KING, WHITE) {
+				moves |= rk2
+			}
+		} else {
+			lk1 := uint64(0b00100000_00000000_00000000_00000000_00000000_00000000_00000000_00000000)
+			lk2 := uint64(0b01000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000)
+			if (lk1|lk2)&ocupied == 0 &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, lk1, b, KING, BLACK) &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, lk2, b, KING, BLACK) {
+				moves |= lk2
+			}
+			rk1 := uint64(0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000)
+			rk2 := uint64(0b00000100_00000000_00000000_00000000_00000000_00000000_00000000_00000000)
+			rk3 := uint64(0b00000010_00000000_00000000_00000000_00000000_00000000_00000000_00000000)
+			if (rk1|rk2|rk3)&ocupied == 0 &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, rk1, b, KING, BLACK) &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, rk2, b, KING, BLACK) &&
+				WouldMyKingBeSafeIfIDidThisComicallyLargeFunctionCall(p.Pos, rk3, b, KING, BLACK) {
+				moves |= rk2
+			}
+		}
+	}
 	return moves
 }
 
